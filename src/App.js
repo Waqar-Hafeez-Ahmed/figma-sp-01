adbab1e0890d2b09eb8334cfa66b2cdf8cdd9e0f
Waqar-Hafeez-Ahmed <you@example.com>
2023-05-01 23:34:51 +0500

    added banner image, logo and hero text

--- src/App.js
+++ src/App.js
@@ -10,3 +10,5 @@ function App() {
 }
 
 export default App;
+
+// https://www.figma.com/file/njDkDgCLxA61YTt0hQAWoM/Final_File_Jobless_Website_1_?node-id=31-753&t=R1VAVr1pNZJQWjvm-0
--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,16 +1,34 @@
 import logo from "../public/svg/logo image.svg";
-import "./HeroSection.css";
+import banner from "../public/assets/woman image.png";
+import ellipse from "../public/assets/Ellipse 39.png";
+import ellipse2 from "../public/assets/Ellipse 40.png";
+import eth from "../public/svg/Union.svg";
 
+import "./HeroSection.css";
 const HeroSection = () => {
   return (
     <div className="background">
-      <div className="herosection">
-        <img src={logo} alt="logo" />
-        <div className="hero-text">
-          <h2>
-            Hire freelancers in confidence with <span>BlockChain </span>
-            technology
-          </h2>
+      <img className="ellipse" src={ellipse} alt="" />
+      <img className="ellipse2" src={ellipse2} alt="" />
+
+      <div className="hero-background">
+        <img className="logo" src={logo} alt="logo" />
+        <div></div>
+        <div className="herosection">
+          <div className="hero-text">
+            <h2>
+              Hire top freelancers in <br />
+              confidence using <img className="union" src={eth} alt="" />
+              <br /> <span>BlockChain </span>
+              technology
+            </h2>
+            <p>
+              Find great talent. Build your business.
+              <br /> Take your career to the next level. finance experts,
+              <br /> product managers, and project managers in the world.
+            </p>
+          </div>
+          <img className="banner-image" src={banner} alt="women" />
         </div>
       </div>
     </div>
