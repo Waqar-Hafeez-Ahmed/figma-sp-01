754e943daeda4fe43ae8edcc3c6858653946927d
Waqar Ahmed <waqarhafeez12@gmail.com>
2023-05-05 16:52:06 +0500

    hero section input feild added

--- src/components/InputFeild.js
+++ src/components/InputFeild.js
@@ -1,10 +1,33 @@
 import React from "react";
+import { useState } from "react";
 import "./InputFeild.css";
+import search from "../public/svg/search icon.svg";
 
 export default function InputFeild() {
+  const [val, setVal] = useState("Search Talent / Project / Job");
+  const changeHandler = (event) => {
+    setVal(event.target.value);
+  };
+  const clickHandler = (event) => {
+    setVal("");
+  };
+  const click = () => {
+    alert(val);
+  };
   return (
     <div>
-      <input className="input2" type="text" />
+      <div className="input"></div>
+      <img className="search" src={search} alt="search" />
+      <input
+        onChange={changeHandler}
+        onClick={clickHandler}
+        className="input2"
+        type="text"
+        value={val}
+      />
+      <button className="hero-btn" onClick={click}>
+        Hire Talent
+      </button>
     </div>
   );
 }
